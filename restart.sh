13b7cfbb607b379f58312f1851f48c9bdda38168
Goaheadz <carlos-m-monica@telecom.pt>
2024-04-21 17:24:05 +0100

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -2,3 +2,5 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 kill $(pgrep -f "python $SCRIPT_DIR/dbus-shelly-em-smartmeter.py")
+chmod a-x $SCRIPT_DIR/service/run
+$SCRIPT_DIR/restart.sh
